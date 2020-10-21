99e1b1d7e7f8efd3f32c37d46eb0a26ef2e71464
alvaro-rodrigues <alvarorbh@hotmail.com>
2020-10-21 14:07:01 -0300

    Commit 18

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,18 +1,4 @@
-import java.util.Enumeration;
-
 public class HtmlStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = s1(aCustomer);
-        while (rentals.hasMoreElements()) {
-           Rental each = (Rental) rentals.nextElement();
-           //show figures for each rental
-           result += s2(each);
-        }
-        //add footer lines
-        result += s3(aCustomer);
-        return result;
-     }
 	public String s1(Customer aCustomer){
 		return "<H1>Rentals for <EM>" + aCustomer.getName() + "</EM></H1><P>\n";
 	}
--- TextStatement.java
+++ TextStatement.java
@@ -1,19 +1,4 @@
-import java.util.Enumeration;
-
 public class TextStatement extends Statement {
-    public String value(Customer aCustomer) {
-        Enumeration rentals = aCustomer.getRentals();
-        String result = s1(aCustomer);
-        while (rentals.hasMoreElements()) {
-           Rental each = (Rental) rentals.nextElement();
-           //show figures for this rental
-           result += s2(each);
-        }
-        //add footer lines
-        result += s3(aCustomer);
-        return result;
-     }
-
 	public String s1(Customer aCustomer){
 		return "Rental Record for " + aCustomer.getName() + "\n";
 	}
